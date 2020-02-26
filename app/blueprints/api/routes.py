0ebe112824710ee352e9764b90327e9845d71c0a
YakuI7 <52265864+YakuI7@users.noreply.github.com>
2020-02-25 22:35:49 -0500

    Added even more custom error messages responses for login and registration forms

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -14,10 +14,12 @@ def login():
     json_data = request.get_json()
     email = json_data['email']
     password = json_data['password']
+    if email == "" and password == "":
+        return jsonify({'email' : "Must not be empty",'password' : "Must not be empty", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
     if email == "":
-        return jsonify({'email' : "Must not be empty", 'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'email' : "Must not be empty", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
     elif password == "":
-        return jsonify({'password' : "Must not be empty", 'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'password' : "Must not be empty", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
     user = guard.authenticate(email, password)
     token = guard.encode_jwt_token(user)
     return jsonify({'access_token' : token})
@@ -28,37 +30,38 @@ def register():
     
     email = json_data['email']
     if email == "":
-        return jsonify({'email' : "Must not be empty", 'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'email' : "Must not be empty",'error' : "AuthenticationError", "message":"Please try again", 'status_code':400}), status.HTTP_400_BAD_REQUEST
 
     password = json_data['password']
     if password == "":
-        return jsonify({'password' : "Must not be empty", 'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'password' : "Must not be empty", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
 
     confirmPassword = json_data['confirmPassword']
     if confirmPassword == "":
-        return jsonify({'confirmPassword' : "Must not be empty", 'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'confirmPassword' : "Must not be empty", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
 
     fname = json_data['fname']
     if fname == "":
-        return jsonify({'fname' : "Must not be empty", 'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'fname' : "Must not be empty", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
 
     lname = json_data['lname']
     if lname == "":
-        return jsonify({'lname' : "Must not be empty", 'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'lname' : "Must not be empty", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
 
     organization_id = json_data['organization_id']
     if organization_id == "":
-        return jsonify({'organization_id' : "Must not be empty", 'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'organization_id' : "Must not be empty", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
 
     user = User(email=email.lower() , fname=fname, lname=lname,
     organization_id=organization_id, password=guard.hash_password(password))
 
     #print(type(user.identify))
     if password != confirmPassword:
-        return jsonify({'error' : "AuthenticationError", "message":"Passwords must match",'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'confirmPassword' : "Passwords must match", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
 
     elif user.lookup(user.email) != None:
-        return jsonify({'error' : "AuthenticationError", "message":"Email is already is use",'status_code':400}), status.HTTP_400_BAD_REQUEST
+        return jsonify({'email' : "Email already in use", 'error' : "AuthenticationError", "message":"Please try again",'status_code':400}), status.HTTP_400_BAD_REQUEST
+        #return jsonify({'error' : "AuthenticationError", "message":"Email is already is use",'status_code':400}), status.HTTP_400_BAD_REQUEST
 
     
     elif password == confirmPassword and user.lookup(user.email) == None:
