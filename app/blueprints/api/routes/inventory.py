b4376f8408734d7fae6c594998cbfdeace72d398
YakuI7 <52265864+YakuI7@users.noreply.github.com>
2020-03-10 11:42:36 -0400

    POST /inventory/<product_id> working

--- app/blueprints/api/routes/inventory.py
+++ app/blueprints/api/routes/inventory.py
@@ -15,21 +15,17 @@ def inventory():
     
     return jsonify(inven)
 
-@api.route('/inventory/<int:beer_id>/modify',methods=['POST'])
-def modifyInventory(beer_id):
-    new_id = beer_id
+@api.route('/inventory/<int:product_id>',methods=['POST'])
+def modifyInventory(product_id):
+    new_id = product_id
 
     json_data = request.get_json()
-    new_sixth = json_data['sixth']
-    new_L50 = json_data['L50']
-    new_half = json_data['half']
-    new_case = json_data['case']
+    new_quantity = json_data['quantity']
 
-    new_inventory = Inventory.query.filter_by(beer_id = new_id).first()
-    new_inventory.sixth = new_sixth
-    new_inventory.L50 = new_L50
-    new_inventory.half = new_half
-    new_inventory.case = new_case
+
+    new_inventory = Product_Inventory.query.filter_by(product_id= new_id).first()
+    new_inventory.quantity= new_quantity
+    
 
     db.session.commit()
     return jsonify({'success' : "Inventory modified successfully"}), status.HTTP_201_CREATED
